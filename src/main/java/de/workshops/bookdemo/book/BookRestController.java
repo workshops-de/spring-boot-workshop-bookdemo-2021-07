e28486340bb34a6ead021d414e4af3e09a5da5a5
Dominik Hirt <dominik.hirt@sigenius.de>
2021-07-12 08:22:57 +0200

    Task: configure swagger

--- src/main/java/de/workshops/bookdemo/book/BookRestController.java
+++ src/main/java/de/workshops/bookdemo/book/BookRestController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -34,10 +33,10 @@ public class BookRestController {
 		return this.bookService.loadBookByIsbn(isbn);
 	}
 	
-	@GetMapping(params = {"author"})
-	public Book getAllBooks(@RequestParam String author) {
-		return this.bookService.loadBookByAuthor(author);
-	}
+//	@GetMapping(params = {"author"})
+//	public Book getAllBooks(@RequestParam(required = false) String author) {
+//		return this.bookService.loadBookByAuthor(author);
+//	}
 	
 	@PostMapping("/search")
 	public List<Book> searchBooks(@RequestBody BookSearchRequest searchRequest) {
